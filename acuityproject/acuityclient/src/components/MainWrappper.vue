6592acc69095c92558079b403f943b04842a395c
Neil <neil.benoit@he-arc.ch>
2022-04-06 09:47:30 +0200

    Commiting capistrano files and modified settings for deployment

---
+++ acuityproject/acuityclient/src/components/MainWrappper.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+        <div class="column main-wrapper">
+            <!-- First column Nothing here -->
+        </div>
+
+        <div class="column">
+            <q-button @click="$router.push('/')" theme="link">Acuity</q-button>
+        </div>
+
+        <div class="column">
+            <q-button @click="$router.push('/myprofile')" theme="secondary" type="icon" icon="" circle></q-button> <!-- TODO Include incon -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainWrapper',
+    data() {
+        return{
+        }
+    },
+    methods: {
+        
+    },
+}
+</script>
