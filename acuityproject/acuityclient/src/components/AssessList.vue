a8612a82c855a02ea03dda06146e851b0a68458f
Neil <neil.benoit@he-arc.ch>
2022-04-13 13:46:51 +0200

    Little modifications

--- acuityproject/acuityclient/src/components/AssessList.vue
+++ acuityproject/acuityclient/src/components/AssessList.vue
@@ -1,7 +1,8 @@
 <template>
 
         <q-scrollbar theme="secondary" class="rounded-border w80">
-            <p v-if="Object.keys(assess).length == 0">Nobody has judged you for now</p>
+            <p v-if="Object.keys(assess).length == 0 && isYou == true">Nobody has judged you for now</p>
+            <p v-else>Nobody has judged him for now</p>
             <div class="w100 comment-box" v-for="a in assess" :key="a">
                 <p :class="{border: a.comment == ''}" class="title">
                     <q-button @click="$router.push('/users/' + a.from_user.id)" theme="link">{{a.from_user.first_name}} {{a.from_user.last_name}}</q-button> 
@@ -25,7 +26,7 @@ export default {
             assess: {},
         }
     },
-    props: ['userId'],
+    props: ['userId', 'isYou'],
     mounted(){
         this.getAssess()
     },
